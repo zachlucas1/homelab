ef6b20c0ac22abfd84abf23202effd07cf5c5992
Zach Lucas <zach.lucas@arivo.com>
2025-06-12 15:21:45 -0600

    backup script update

--- minecraft/gracie/minecraft_backup.sh
+++ minecraft/gracie/minecraft_backup.sh
@@ -1,26 +1,40 @@
 #!/bin/bash
+# daily_backup.sh
+# Creates and uploads a daily backup of the Minecraft server, then deletes it locally
 
-# Set date and filename
-FILENAME="gracie_minecraft_backup.zip"
-BACKUP_DIR="/home/opc"
+set -e
 
-#cd to minecraft directory
-cd $BACKUP_DIR
+CONTAINER_NAME="minecraft-server"
+TEMP_COPY="/tmp/minecraft_server"
+WORLD_PATH="/home/opc/minecraft_server"
+ARCHIVE_PATH="/tmp/$CONTAINER_NAME-latest.tar.gz"
+REMOTE_PATH="gracie-b2:gracie-minecraft-backup"
 
-#stop minecraft server
-sudo systemctl stop minecraft.service
+echo "--DAILY BACKUP--"
 
-#wait 15 seconds
-sleep 15
+# Pause saving
+docker exec "$CONTAINER_NAME" rcon-cli save-off
+docker exec "$CONTAINER_NAME" rcon-cli save-all flush
+sleep 3
 
-#compress files into .zip
-zip -r "$FILENAME" *
+# Copy world data
+rm -rf "$TEMP_COPY"
+cp -r "$WORLD_PATH" "$TEMP_COPY"
 
-#backup file
-rclone copy $FILENAME gracie-b2:gracie-minecraft-backup
+# Resume saving
+docker exec "$CONTAINER_NAME" rcon-cli save-on
 
-#remove backup file
-rm $FILENAME
+# Create compressed archive in /tmp
+tar czf "$ARCHIVE_PATH" -C "$TEMP_COPY" .
 
-#stop minecraft server
-sudo systemctl start minecraft.service
+# Clean up temp copy
+rm -rf "$TEMP_COPY"
+
+# Upload to Backblaze
+echo "Uploading to Backblaze..."
+rclone copy "$ARCHIVE_PATH" "$REMOTE_PATH"
+
+# Remove local archive
+rm -f "$ARCHIVE_PATH"
+
+echo "--BACKUP COMPLETE--"
